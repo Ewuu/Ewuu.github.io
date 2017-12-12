2e26522e44b1ced0f171925746aac4c60e7635a9
Ewuu <31820561+Ewuu@users.noreply.github.com>
2017-12-12 20:28:20 +0100

    Add files via upload

---
+++ time.js
@@ -0,0 +1,21 @@
+﻿function time() {
+  var data = new Date();
+
+  var day = data.getDate();
+  var month = data.getMonth() + 1;
+  var year = data.getFullYear();
+
+  var hour = data.getHours();
+  var minute = data.getMinutes();
+  var second = data.getSeconds();
+
+  if (minute < 10) minute = "0" + minute;
+  if (second < 10) second = "0" + second;
+
+  document.getElementById("zegarLayer").innerHTML =
+    day + "." + month + "." + year + " | " + hour + ":" + minute + ":" + second;
+
+
+}
+
+setInterval("time()", 1000);
